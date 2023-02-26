84410087df928506fa9131ad71bf419bbcc61e70
Keith Gibson <keith@Keiths-Mac-mini.local>
2023-02-26 19:37:29 +0000

    Setup components

---
+++ src/components/EmployeeCard/EmployeeCard.js
@@ -0,0 +1,7 @@
+import {useContext} from 'react';
+
+function EmployeeCard() {
+  return <div></div>;
+}
+
+export default EmployeeCard;
---
+++ src/components/EmployeeCard/index.js
@@ -0,0 +1,2 @@
+export * from './EmployeeCard';
+export { default } from './EmployeeCard';
